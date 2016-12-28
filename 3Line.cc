899ca0c9a01c58fab32ad3f2ac2d4bc970531b2a
AntonioManuelGarcia <amgf9@alu.ua.es>
2016-12-28 11:56:44 +0100

    v1.2.0

--- 3Line.cc
+++ 3Line.cc
@@ -6,8 +6,9 @@ int main(){
 
 	char Tablero[3][3];
 	bool final = false;
+	bool jugador = false;
 	char jugada;
-	int x,y;
+	int x,y,cont=0;
 	for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
 				
@@ -16,8 +17,9 @@ int main(){
 		}
 
 	do{
-		cout << "indique su jugada: ";
-		cin >> jugada;
+		jugador=cont%2;
+		cout << "jugador: " << (jugador+1) << endl;
+		
 		cout << endl << "indique la Coordenada x (1-3): ";
 		cin >> x;
 		while(x<1 || x>3){
@@ -30,7 +32,8 @@ int main(){
 			cout << endl << "COORDENADA INCORRECTA! LA COORDENADA DEBE SER UN NUMERO ENTRE 1 Y 3" << endl << "Por favor indique la Coordenada y: ";
 			cin >> y;
 		}
-		Tablero[(x-1)][(y-1)]=jugada;
+		if(jugador==0){Tablero[(x-1)][(y-1)]='X';}
+		else{Tablero[(x-1)][(y-1)]='O';}
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
 				
@@ -41,7 +44,7 @@ int main(){
 		
 		cout << "Desea acabar el juego? (s/n) ";
 		cin >> jugada;
-		if(jugada=='s'){ final=true;}
+		if(jugada=='s'){ final=true;}else{cont++;}
 	}while(final!=true);
 
 
