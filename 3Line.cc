fad463daa9d74ff4598fe3e1f8a0dd5a0b174be7
AntonioManuelGarcia <amgf9@alu.ua.es>
2016-12-28 11:34:52 +0100

    codigo de la V1.1.0

--- 3Line.cc
+++ 3Line.cc
@@ -6,8 +6,9 @@ int main(){
 
 	char Tablero[3][3];
 	bool final = false;
+	bool jugador = false;
 	char jugada;
-	int x,y;
+	int x,y,cont=0;
 	for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
 				
@@ -16,13 +17,23 @@ int main(){
 		}
 
 	do{
+		jugador=cont%2;
+		cout << "jugador: " << jugador << endl;
 		cout << "indique su jugada: ";
 		cin >> jugada;
-		cout << endl << "indique la Coordenada x: ";
+		cout << endl << "indique la Coordenada x (1-3): ";
 		cin >> x;
-		cout << endl << "indique la Coordenada y: ";
+		while(x<1 || x>3){
+			cout << endl << "COORDENADA INCORRECTA! LA COORDENADA DEBE SER UN NUMERO ENTRE 1 Y 3" << endl << "Por favor indique la Coordenada x: ";
+			cin >> x;
+		}
+		cout << endl << "indique la Coordenada y (1-3): ";
 		cin >> y;
-		Tablero[x][y]=jugada;
+		while(y<1 || y>3){
+			cout << endl << "COORDENADA INCORRECTA! LA COORDENADA DEBE SER UN NUMERO ENTRE 1 Y 3" << endl << "Por favor indique la Coordenada y: ";
+			cin >> y;
+		}
+		Tablero[(x-1)][(y-1)]=jugada;
 		for(int i=0;i<3;i++){
 			for(int j=0;j<3;j++){
 				
@@ -33,7 +44,7 @@ int main(){
 		
 		cout << "Desea acabar el juego? (s/n) ";
 		cin >> jugada;
-		if(jugada=='s'){ final=true;}
+		if(jugada=='s'){ final=true;}else{cont++;}
 	}while(final!=true);
 
 
